f25c9166c805ec6dfba73a9ac8bce49f93423d9e
Rizialdi <rasselio.diack@gmail.com>
2017-09-15 05:06:24 +0200

    Added localStorage to our app

--- src/reducers/listOfTodo.js
+++ src/reducers/listOfTodo.js
@@ -1,9 +1,23 @@
-export default function listOfTodo(state = [], action) {
+export default function listOfTodo(state = ilo(), action) {
     switch (action.type) {
         //dans le cas ou on click Add Item on action avec ce type = 'ADD_TODO' est envoyee
         //et le nouveau state est lancien avec au dessus le nouvel arrivant
         case 'ADD_TODO':
-            return [action.payload, ...state]
+
+
+            // block de code pour creer le localStorage et l'Updater
+            if(localStorage.getItem("todos") === null){
+                let todos = [];
+                todos.unshift(action.payload);
+                localStorage.setItem("todos", JSON.stringify(todos))
+            }else {
+                let todos = JSON.parse(localStorage.getItem("todos"))
+                todos.unshift(action.payload)
+                localStorage.setItem("todos", JSON.stringify(todos))
+            }
+
+
+            return [...JSON.parse(localStorage.getItem("todos"))]
         case 'REMOVE_TODO':
             //utilisation de la fonction filter qui retourne tous les elements  verifiant la condition
             //jai mis la negation pour retourner tout ce dont lidentifiant ne correspond pas
@@ -13,6 +27,9 @@ export default function listOfTodo(state = [], action) {
             return state
     }
 }
+// il faut englober lexpression !(localStorage.getItem("todos") === null) a evaluer
+let ilo = () => ( !(localStorage.getItem("todos") === null) ? [...JSON.parse(localStorage.getItem("todos"))] : [] )
+
 
 /*
 Un reducer prend comme argument le state courant et une action(action.type
--- src/store.js
+++ src/store.js
@@ -6,6 +6,6 @@ Il faut creer un store pour notree application
 Ce store est fait g'race a la fonction createStore et
 prend comme arguement un reducer et un etat initial pour le store
 il ya un object avec un array a l'interieur nommé listOfTodo
-C'est listOfTodo qui sera rempli avec chaque todoItem
+C'est listOfTodo qui sera rempli avec chaque
 */
-export default createStore(reducer, {listOfTodo: []});
+export default createStore(reducer);
